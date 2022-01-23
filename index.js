201d8ee555c7c45b44bf45fae54195cf08ac9255
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-23 15:13:21 +0100

    Refactor main to avoid having to mock modules

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const core = require("@actions/core");
+const childProcess = require("child_process");
 const os = require("os");
+const { env } = require("process");
+const shescape = require("shescape");
 
 const main = require("./src/main.js");
 
 const platform = os.platform();
-main(platform);
+
+main({
+  childProcess,
+  core,
+  env,
+  platform,
+  shescape,
+});
