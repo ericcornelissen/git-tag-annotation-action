4073b0cc472ca115dce259177ebf1fc426fa51a4
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-23 16:47:37 +0100

    Rewrite tests using AVA
    Remove Jest (dependency and mocks) and rewrite the test suite using
    AVA [1].
    
    TODOs:
    - Update the `coverage` script
    - Update the `test:mutation` script
    
    --
    1. https://github.com/avajs/ava

--- test/main.test.js
+++ test/main.test.js
@@ -1,125 +1,190 @@
-const core = require("@actions/core");
-const child_process = require("child_process");
-const shescape = require("shescape");
+const test = require("ava");
+const sinon = require("sinon");
 
 const main = require("../src/main.js");
-
-jest.mock("@actions/core");
-jest.mock("child_process");
-jest.mock("shescape");
 
 const linux = "linux";
 const win32 = "win32";
 
-describe.each([linux, win32])("os: %s", (platform) => {
-  beforeEach(() => {
-    core.getInput.mockClear();
-    core.setFailed.mockClear();
-    core.setOutput.mockClear();
+test.beforeEach((t) => {
+  t.context.childProcess = {
+    exec: sinon.stub(),
+  };
 
-    child_process.exec.mockClear();
+  t.context.core = {
+    getInput: sinon.stub(),
+    setFailed: sinon.stub(),
+    setOutput: sinon.stub(),
+  };
 
-    shescape.quote.mockClear();
-  });
+  t.context.env = {
+    GITHUB_REF: "v1.0.0",
+  };
 
-  it.each(["v1.2.3", "v0.3.14"])(
-    "uses the tag from the environment (%s)",
-    (tag) => {
-      process.env.GITHUB_REF = `refs/tags/${tag}`;
-
-      main(platform);
-
-      expect(child_process.exec).toHaveBeenCalledWith(
-        expect.stringContaining(`'refs/tags/${tag}'`),
-        expect.any(Function)
-      );
-    }
-  );
-
-  it("tries to get a tag from the input", () => {
-    core.getInput.mockReturnValueOnce(undefined);
-
-    main(platform);
-
-    expect(core.getInput).toHaveBeenCalledTimes(1);
-    expect(core.getInput).toHaveBeenCalledWith("tag");
-  });
-
-  it.each(["v3.2.1", "v0.2.718"])("uses the tag from the input (%s)", (tag) => {
-    core.getInput.mockReturnValueOnce(tag);
-
-    main(platform);
-
-    expect(core.getInput).toHaveBeenCalledTimes(1);
-    expect(child_process.exec).toHaveBeenCalledWith(
-      expect.stringContaining(`'refs/tags/${tag}'`),
-      expect.any(Function)
-    );
-  });
-
-  it("uses the correct format", () => {
-    let expectedFormat;
-    if (platform === win32) {
-      expectedFormat = "%(contents)";
-    } else {
-      expectedFormat = "'%(contents)'";
-    }
-
-    main(platform);
-
-    expect(core.getInput).toHaveBeenCalledTimes(1);
-    expect(child_process.exec).toHaveBeenCalledWith(
-      expect.stringContaining(`--format=${expectedFormat}`),
-      expect.any(Function)
-    );
-  });
-
-  it("outputs the annotation", (done) => {
-    const annotation = "Hello world!";
-    child_process.exec.mockImplementationOnce((_, fn) => {
-      fn(null, annotation);
-
-      expect(core.setOutput).toHaveBeenCalledTimes(1);
-      expect(core.setOutput).toHaveBeenCalledWith(
-        "git-tag-annotation",
-        annotation
-      );
-      done();
-    });
-
-    main(platform);
-  });
-
-  it("sets an error if the annotation could not be found", (done) => {
-    child_process.exec.mockImplementationOnce((_, fn) => {
-      fn("Something went wrong!", null);
-
-      expect(core.setOutput).not.toHaveBeenCalled();
-      expect(core.setFailed).toHaveBeenCalledTimes(1);
-      done();
-    });
-
-    main(platform);
-  });
-
-  it("sets an error if exec fails", () => {
-    child_process.exec.mockImplementationOnce(() => {
-      throw new Error({ message: "Something went wrong" });
-    });
-
-    main(platform);
-
-    expect(core.setOutput).not.toHaveBeenCalled();
-    expect(core.setFailed).toHaveBeenCalledTimes(1);
-  });
-
-  it("escapes malicious values from the input", () => {
-    const tag = `'; $(cat /etc/shadow)`;
-    core.getInput.mockReturnValueOnce(tag);
-
-    main(platform);
-
-    expect(shescape.quote).toHaveBeenCalledTimes(1);
-    expect(shescape.quote).toHaveBeenCalledWith(`refs/tags/${tag}`);
-  });
+  t.context.shescape = {
+    quote: sinon.stub(),
+  };
 });
+
+const testGetInput = test.macro({
+  exec(t, platform) {
+    main({ ...t.context, platform });
+
+    t.true(t.context.core.getInput.calledOnce);
+    t.true(t.context.core.getInput.calledWith("tag"));
+  },
+  title(_, platform) {
+    return `get input at key "tag" on ${platform}`;
+  },
+});
+
+const testGitCommand = test.macro({
+  exec(t, platform) {
+    main({ ...t.context, platform });
+
+    t.true(t.context.childProcess.exec.calledOnce);
+    t.true(
+      t.context.childProcess.exec.calledWith(
+        sinon.match(/^git for-each-ref .+$/),
+        sinon.match.func
+      )
+    );
+  },
+  title(_, platform) {
+    return `git command that is executed on ${platform}`;
+  },
+});
+
+const testGitCommandEnv = test.macro({
+  exec(t, platform, tag) {
+    const ref = `refs/tags/${tag}`;
+    const escapedRef = `'${ref}'`;
+
+    t.context.core.getInput.returns(undefined);
+    t.context.env.GITHUB_REF = ref;
+    t.context.shescape.quote.returns(escapedRef);
+
+    main({ ...t.context, platform });
+
+    t.true(t.context.shescape.quote.calledWith(ref));
+    t.true(
+      t.context.childProcess.exec.calledWith(
+        sinon.match(escapedRef),
+        sinon.match.func
+      )
+    );
+  },
+  title(_, platform, tag) {
+    return `uses the tag from the environment on ${platform}, tag=${tag}`;
+  },
+});
+
+const testGitCommandInput = test.macro({
+  exec(t, platform, input) {
+    const ref = `refs/tags/${input}`;
+    const escapedRef = `'${ref}'`;
+
+    t.context.core.getInput.returns(input);
+    t.context.shescape.quote.returns(escapedRef);
+
+    main({ ...t.context, platform });
+
+    t.true(t.context.shescape.quote.calledWith(ref));
+    t.true(
+      t.context.childProcess.exec.calledWith(
+        sinon.match(escapedRef),
+        sinon.match.func
+      )
+    );
+  },
+  title(_, platform, tag) {
+    return `uses the tag from the input ${platform}, tag=${tag}`;
+  },
+});
+
+const testGitCommandFormatArgument = test.macro({
+  exec(t, platform, expected) {
+    main({ ...t.context, platform });
+
+    t.true(
+      t.context.childProcess.exec.calledWith(
+        sinon.match(`--format=${expected}`),
+        sinon.match.func
+      )
+    );
+  },
+  title(_, platform) {
+    return `uses the correct for-each-ref format on ${platform}`;
+  },
+});
+
+const testOutputExecSuccess = test.macro({
+  exec(t, platform, annotation) {
+    main({ ...t.context, platform });
+
+    t.true(t.context.childProcess.exec.calledOnce);
+    t.context.childProcess.exec.lastCall.callback(null, annotation);
+
+    t.false(t.context.core.setFailed.called);
+    t.true(t.context.core.setOutput.calledOnce);
+    t.true(
+      t.context.core.setOutput.calledWith("git-tag-annotation", annotation)
+    );
+  },
+  title(_, platform, annotation) {
+    return `command success ${platform}, annotation="${annotation}"`;
+  },
+});
+
+const testOutputExecFailure = test.macro({
+  exec(t, platform, err) {
+    main({ ...t.context, platform });
+
+    t.true(t.context.childProcess.exec.calledOnce);
+    t.context.childProcess.exec.lastCall.callback(err, null);
+
+    t.false(t.context.core.setOutput.called);
+    t.true(t.context.core.setFailed.calledOnce);
+    t.true(t.context.core.setFailed.calledWith(err));
+  },
+  title(_, platform, err) {
+    return `command error on ${platform}, err="${err}"`;
+  },
+});
+
+const testOutputFailure = test.macro({
+  exec(t, platform, err) {
+    t.context.childProcess.exec.throws(new Error(err));
+
+    main({ ...t.context, platform });
+
+    t.false(t.context.core.setOutput.called);
+    t.true(t.context.core.setFailed.calledOnce);
+    t.true(t.context.core.setFailed.calledWith(err));
+  },
+  title(_, platform, err) {
+    return `execution error on ${platform}, err="${err}"`;
+  },
+});
+
+for (const platform of [linux, win32]) {
+  test(testGetInput, platform);
+  test(testGitCommand, platform);
+
+  for (const tag of ["v3.1.4", "v0.2.718", "v0.0.1"]) {
+    test(testGitCommandEnv, platform, tag);
+    test(testGitCommandInput, platform, tag);
+  }
+
+  for (const annotation of ["Hello world!", "foobar"]) {
+    test(testOutputExecSuccess, platform, annotation);
+  }
+
+  for (const err of ["Something went wrong", "Oops"]) {
+    test(testOutputExecFailure, platform, err);
+    test(testOutputFailure, platform, err);
+  }
+}
+
+test(testGitCommandFormatArgument, win32, "%(contents)");
+test(testGitCommandFormatArgument, linux, "'%(contents)'");
