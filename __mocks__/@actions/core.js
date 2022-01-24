4b10eae4ad3fb60ad2a02ff461020ce42d5c1ba0
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-24 12:03:46 +0100

    Rewrite tests using uvu
    Remove Jest (dependency and mocks) and rewrite the test suite using
    uvu [1] and Sinon.JS [2].
    
    TODOs:
    - Update the `coverage` script
    - Update the `test:mutation` script
    
    --
    1. https://github.com/lukeed/uvu
    2. https://sinonjs.org

--- __mocks__/@actions/core.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const core = jest.createMockFromModule("@actions/core");
-
-core.getInput = jest.fn().mockName("core.getInput");
-core.setFailed = jest.fn().mockName("core.setFailed");
-core.setOutput = jest.fn().mockName("core.setOutput");
-
-module.exports = core;
