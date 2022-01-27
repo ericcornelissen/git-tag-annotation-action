121c1f4eb5642c8046ec3e41712ab0ef3f77ae3a
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-27 17:34:52 +0100

    Update git hook formatting
    Prefer the use of
    
        if [ ... ]; then
    
    over
    
        if [ ... ]
        then
    
    because it's fewer lines and follows the if-formatting used in the
    JavaScript code of the project (`if (...) {` not `if (...)\n{`).

--- script/hooks/common.sh
+++ script/hooks/common.sh
@@ -4,8 +4,7 @@ MERGE_HEAD="$(git rev-parse --git-dir)/MERGE_HEAD"
 
 get_stash_count () {
   local count=$(git rev-list --walk-reflogs --count refs/stash 2> /dev/null)
-  if [ "$count" = "" ]
-  then
+  if [ "$count" = "" ]; then
     echo "0"
   else
     echo $count
