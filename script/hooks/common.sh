fd47a25818c44b2a035ab98b1497cbbb59f15367
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-27 17:29:42 +0100

    Don't create a stash during merging
    Update the git hooks to *not* create a stash during merging, this is
    because stashing during merging messes up the merge process. So, now it
    is alwasy necessary to use the `--no-verify` flag to disable the hooks
    when committing a merge.

--- script/hooks/common.sh
+++ script/hooks/common.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+MERGE_HEAD="$(git rev-parse --git-dir)/MERGE_HEAD"
+
 get_stash_count () {
   local count=$(git rev-list --walk-reflogs --count refs/stash 2> /dev/null)
   if [ "$count" = "" ]
