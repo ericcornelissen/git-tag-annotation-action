b1097c9d13e475ea8ab9b8aedd784f88759a8898
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-28 23:56:36 +0100

    Improve git hooks (#88)
    * Don't create a stash during merging
    
    Update the git hooks to *not* create a stash during merging, this is
    because stashing during merging messes up the merge process. So, now it
    is alwasy necessary to use the `--no-verify` flag to disable the hooks
    when committing a merge.
    
    * Update git hook formatting
    
    Prefer the use of
    
        if [ ... ]; then
    
    over
    
        if [ ... ]
        then
    
    because it's fewer lines and follows the if-formatting used in the
    JavaScript code of the project (`if (...) {` not `if (...)\n{`).
    
    * Stash unstaged *and* staged changes before pushing
    
    In contrast to when you're committing, when you're pushing we want to
    know if the current state of the project is "correct" (according to the
    checks in the hook). (When committing we want to know if the next state,
    including the staged changes, is "correct").

--- script/hooks/common.sh
+++ script/hooks/common.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+MERGE_HEAD="$(git rev-parse --git-dir)/MERGE_HEAD"
+
 get_stash_count () {
   local count=$(git rev-list --walk-reflogs --count refs/stash 2> /dev/null)
-  if [ "$count" = "" ]
-  then
+  if [ "$count" = "" ]; then
     echo "0"
   else
     echo $count
