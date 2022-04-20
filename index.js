97cfab36b1bbfd2b87875ad4778f7defa37a58af
Eric Cornelissen <ericornelissen@gmail.com>
2022-04-20 10:00:23 +0200

    Use `node:` imports (#164)
    Update the source code to use `node:` imports (see [1] for more details)
    so that these are valid absolute URLs and cannot accidentally be
    overriden by a third-party dependency.
    
    The rollup configuration is updated to explicitly configure `node:`
    imports as being external (this wasn't automatically recognized).
    
    --
    1. https://nodejs.org/api/esm.html#esm_node_imports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
-const childProcess = require("child_process");
-const os = require("os");
-const { env } = require("process");
+const childProcess = require("node:child_process");
+const os = require("node:os");
+const { env } = require("node:process");
 const shescape = require("shescape");
 
 const main = require("./src/main.js");
