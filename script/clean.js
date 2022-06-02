c44b27e368fb43d4acb5154df9a71d97f87ebd60
Eric Cornelissen <ericornelissen@gmail.com>
2022-06-03 00:05:06 +0200

    Use `node:` imports in scripts
    Update the scripts to use `node:` imports (see [1] for more details)
    so that these are valid absolute URLs and cannot accidentally be
    overriden by a third-party dependency.
    --
    1. https://nodejs.org/api/esm.html#esm_node_imports

--- script/clean.js
+++ script/clean.js
@@ -1,6 +1,6 @@
-const cp = require("child_process");
-const fs = require("fs");
-const path = require("path");
+const cp = require("node:child_process");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const folders = ["./.temp", "./_reports"];
 
