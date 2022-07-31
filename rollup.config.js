f164e2822e1b117fd1d0aea908c1aa8e7a2c400d
Eric Cornelissen <ericornelissen@gmail.com>
2022-07-31 14:07:56 +0200

    Rewrite project in ESM (#259)

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-const commonjs = require("@rollup/plugin-commonjs");
-const { nodeResolve } = require("@rollup/plugin-node-resolve");
-const { terser } = require("rollup-plugin-terser");
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
-module.exports = {
+export default {
   input: "index.js",
   output: {
-    exports: "default",
-    file: "lib/index.js",
+    file: "lib/index.cjs",
     format: "cjs",
   },
   external: ["node:child_process", "node:os", "node:process"],
