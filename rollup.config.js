923289e344f2771328d632b0fc8a0e6ff456d0d2
Eric Cornelissen <ericornelissen@gmail.com>
2022-07-31 13:09:19 +0200

    Update bundling for ESM source
    Update the Rollup configuration so that it can bundle the ESM source
    code into a standalone "executable" JavaScript file. Bundling still
    targets a CJS file, so the Action manifest has been updated to run the
    generated CJS file.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
-const commonjs = require("@rollup/plugin-commonjs");
-const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const { terser } = require("rollup-plugin-terser");
 
 module.exports = {
   input: "index.js",
   output: {
-    exports: "default",
-    file: "lib/index.js",
+    file: "lib/index.cjs",
     format: "cjs",
   },
   external: ["node:child_process", "node:os", "node:process"],
-  plugins: [commonjs(), nodeResolve(), terser()],
+  plugins: [terser()],
 };
