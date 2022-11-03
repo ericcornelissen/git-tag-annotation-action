f18cbb96c12e90089cb10689a9d3ada3f18695f5
Eric Cornelissen <ericornelissen@gmail.com>
2022-11-03 23:14:22 +0100

    Replace `rollup-plugin-terser` with `@rollup/plugin-terser` (#375)
    The latter is preferred over the former as it's more recent and up-to-date.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "index.js",
