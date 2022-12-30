fb460aaec6c79fea12e8af154e3855ffb77f2d40
Eric Cornelissen <ericornelissen@gmail.com>
2022-12-30 16:38:45 +0100

    Format `bump-version.mjs`

--- script/bump-version.mjs
+++ script/bump-version.mjs
@@ -7,26 +7,24 @@ const manifestFile = path.resolve("./.version");
 const manifestRaw = fs.readFileSync(manifestFile).toString();
 const version = manifestRaw.trim();
 
-const tmp = version.split(".");
-
-const bump = process.argv[2]
-switch (bump) {
-case "patch":
-  tmp[2] = parseInt(tmp[2], 10) + 1
-  break;
-case "minor":
-  tmp[1] = parseInt(tmp[1], 10) + 1
-  tmp[2] = 0
-  break;
-case "major":
-  tmp[0] = parseInt(tmp[0], 10) + 1
-  tmp[1] = 0
-  tmp[2] = 0
-  break;
-default:
-  console.log(`unknown argument '${bump}'`);
-  console.log(`must be one of 'patch', 'minor', 'major'`);
-  break;
+const newVersion = version.split(".");
+switch (process.argv[2]) {
+  case "patch":
+    newVersion[2] = parseInt(newVersion[2], 10) + 1;
+    break;
+  case "minor":
+    newVersion[1] = parseInt(newVersion[1], 10) + 1;
+    newVersion[2] = 0;
+    break;
+  case "major":
+    newVersion[0] = parseInt(newVersion[0], 10) + 1;
+    newVersion[1] = 0;
+    newVersion[2] = 0;
+    break;
+  default:
+    console.log(`unknown argument '${bump}'`);
+    console.log(`must be one of 'patch', 'minor', 'major'`);
+    break;
 }
 
-fs.writeFileSync(manifestFile, `${tmp.join(".")}\n`);
+fs.writeFileSync(manifestFile, `${newVersion.join(".")}\n`);
